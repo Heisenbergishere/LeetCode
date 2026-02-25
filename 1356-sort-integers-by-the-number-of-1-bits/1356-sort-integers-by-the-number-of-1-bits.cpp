01f763ad72ce1ae0e4b2f9ab574109369ae7234a
Heisenbergishere <karanthakur954819@gmail.com>
2026-02-25 09:30:51 +0530

    Time: 7 ms (19.88%), Space: 15 MB (16.23%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-long long  setbits(int n){
-    long long ans=0;
-    while(n){
-        if( n%2 ==1)ans++;
-        n/=2;
-    }
-    return ans;
-}
+
     vector<int> sortByBits(vector<int>& arr) {
         vector<int>ans;
         int n =arr.size();
@@ -15,7 +8,7 @@ long long  setbits(int n){
         while(j<=14){
             vector<long long >v;
             for( int i =0;i<n;i++){
-                if( setbits(arr[i])==j ){
+                if( __builtin_popcount(arr[i])==j ){
                     v.push_back(arr[i]);
                 }
             }
