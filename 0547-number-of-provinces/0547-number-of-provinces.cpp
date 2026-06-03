075c5baf64b0b3054fd65b500a8a849460cb110b
Heisenbergishere <karanthakur954819@gmail.com>
2026-06-03 15:03:56 +0530

    Time: 8 ms (8.48%), Space: 22.2 MB (5.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,41 @@
 class Solution {
-private :
-    void dfs( vector<vector<int>>adjls , vector<int>&vis,int k){
-        vis[k]=1;
-        for( auto it : adjls[k] ){
-            if( !vis[it]){
-                dfs(adjls,vis,it);
-            }
-        }
-}
 public:
+void dfs( vector<bool>&vis ,vector<vector<int>>&adj,int i,int n){
+    queue<int>q;
+    q.push(i);
+    vis[i]=true;
+    while(q.size()){
+        int k = q.front();
+        q.pop();
+        auto vec = adj[k];
+       for( int j=0;j<vec.size();j++){
+           if( !vis[vec[j]]){
+            q.push(vec[j]);
+            vis[vec[j]]=true;
+           }
+        }
+    }
+}
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        //to change adjacency matrix to list
-        vector<vector<int>>adjls(n);
+        vector<vector<int>>adj(n+1);
+        vector<bool>vis(n+1,false);
+        vis[0]=true;
         for( int i =0;i<n;i++){
             for( int j =0;j<n;j++){
-                if( isConnected[i][j] ==1 && i !=j){
-                    adjls[i].push_back(j);
-                    adjls[j].push_back(i);
-                }
+                if( isConnected[i][j]){
+                    adj[i+1].push_back(j+1);
+                    adj[j+1].push_back(i+1);
+                }   
             }
         }
-         vector<int> vis(n,0);
-        int count =0;
-        for( int i =0;i<n;i++){
-            if( !vis[i]){
-                count++;
-                dfs(adjls,vis , i);
+        int ans=0;
+        for( int i=0;i<n;i++){
+            if( !vis[i+1]){
+                dfs(vis,adj,i+1,n);
+                ans++;
             }
         }
-        return count;
+    return ans;
     }
 };
