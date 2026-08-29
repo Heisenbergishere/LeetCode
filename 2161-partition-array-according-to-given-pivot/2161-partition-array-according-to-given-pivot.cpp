b466b66e10cbf87721f14d519fe0c19d7884bf56
Heisenbergishere <karanthakur954819@gmail.com>
2026-08-29 12:20:13 +0530

    Time: 8 ms (48.4%), Space: 127.5 MB (97.28%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n=nums.size();
-        vector<int >vec;
-        for ( int i =0; i<n;i++){
-            if ( nums[i] < pivot){
-                vec.push_back(nums[i]);
+        int n = nums.size();
+        vector<int>v(n,0);
+        int j=0;
+        for( int i =0;i<n;i++){
+            if( nums[i] < pivot){
+                v[j]=nums[i];
+                j++;
             }
         }
-        for ( int i =0; i<n;i++){
-            if ( nums[i] == pivot){
-                vec.push_back(nums[i]);
+        for( int i =0;i<n;i++){
+            if( nums[i] == pivot){
+                v[j]=nums[i];
+                j++;
             }
         }
-        for ( int i =0; i<n;i++){
-            if ( nums[i] > pivot){
-                vec.push_back(nums[i]);
+        for( int i =0;i<n;i++){
+            if( nums[i] > pivot){
+                v[j]=nums[i];
+                j++;
             }
         }
-        for ( int k =0;k<nums.size();k++){
-            nums[k] = vec[k];
-        }
-    return nums;
+        return v;
     }
 };
